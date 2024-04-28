aa5a6de1b176134b96deb2f91e94d7b76b440d23
HermannSV <41011144+HermannSV@users.noreply.github.com>
2024-04-28 17:32:08 -0600

    Portfolio 1.0
    Main structure and design for portfolio, pending link to projects repositories & demos.

---
+++ app.js
@@ -0,0 +1,7 @@
+/*function redirectImg1() {
+    windows.location.href = "https://hermannsv.github.io/barberia-alura/";
+}*/
+
+function repository() {
+    windows.location = "https://hermannsv.github.io/barberia-alura/";
+}
